38f80ae7b59d66520c152d3e7eb380fdd3d72c33
nadere <naderezaboli@gmail.com>
2025-05-07 16:38:38 +0330

    #

--- src/redux/domainApi.js
+++ src/redux/domainApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const domainApi = createApi({
   reducerPath: "domainApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://6797aa2bc2c861de0c6d964c.mockapi.io/domain", // ✅ No trailing slash
+    baseUrl: "https://6797aa2bc2c861de0c6d964c.mockapi.io/domain", 
   }),
   tagTypes: ["Domain"],
   endpoints: (builder) => ({
